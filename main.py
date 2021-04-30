6dd637b67701c0e12f68979d8b6e9ace49535c1d
Daniel Lawson <daniellawson9999@gmail.com>
2021-04-30 13:49:50 -0400

    added both plots to main

--- main.py
+++ main.py
@@ -7,6 +7,91 @@ import preprocess
 import warnings
 import sys
 from plot import hyperparam_plot
+import numpy as np
+import matplotlib.pyplot as plt
+
+def create_hyperparameter_plots():
+    total_err, total_err2, err_dict_svm, err_dict_knn = run_single(1000)
+    hyperparam_plot('SVM Error for different slack values (C)', 'C', err_dict_svm)
+    hyperparam_plot('KNN Error for different nearest-neighbor values (k)', 'k', err_dict_knn)
+
+
+def create_size_plots():
+    # dictionaries for different graphs of C hyperparameters and K hypyerpamaters
+    err_C_01 = {}
+    err_C_1 = {}
+    err_C_10 = {}
+    err_K_5 = {}
+    err_K_10 = {}
+    err_K_15 = {}
+
+    size = 0
+    total_errSVC = np.empty(5)
+    total_errKNN = np.empty(5)
+    sample_sizes = np.array([200, 400, 600, 800, 1000])
+    # get error cross validation between sizes 200-1000 at intervals of 200
+    for i in range(5):
+        size += 200
+
+        # get the svm for error dictionary
+        total_errSVC[i], total_errKNN[i], err_dict_svm, err_dict_knn = run_single(size)
+
+        # assign key, value pair for current size, svm error for each C hyperparameter
+        for k, v in err_dict_svm.items():
+            if k == 0.1:
+                err_C_01[size] = v
+            elif k == 1:
+                err_C_1[size] = v
+            else:
+                err_C_10[size] = v
+
+        # perform same process for KNN
+        for k, v in err_dict_knn.items():
+            if k == 5:
+                err_K_5[size] = v
+            elif k == 10:
+                err_K_10[size] = v
+            else:
+                err_K_15[size] = v
+
+    # print("FINAL: ")
+    # print(err_C_01)
+    # print(err_C_1)
+    # print(err_C_10)
+
+    # plot the graphs with different values of hyperparameters]
+
+    hyperparam_plot('SVM Error for slack value 0.1 for different sizes', 'Size', err_C_01)
+    hyperparam_plot('SVM Error for slack value 1 for different sizes', 'Size', err_C_1)
+    hyperparam_plot('SVM Error for slack value 10 for different sizes', 'Size', err_C_10)
+    hyperparam_plot('KNN Error for K value 5 for different sizes', 'Size', err_K_5)
+    hyperparam_plot('KNN Error for K value 10 for different sizes', 'Size', err_K_10)
+    hyperparam_plot('KNN Error for K value 15 for different sizes', 'Size', err_K_15)
+
+def run_single(size):
+    if not sys.warnoptions:
+        warnings.simplefilter("ignore")
+
+    # fix seed for testing purposes
+    random.seed(10)
+
+    # run the preprocessing to get the subset of data
+    X, y = preprocess.run("review_polarity.tar.gz", 0.05, size, binary=False)
+    y = y.to_numpy(dtype=int)
+
+    # shuffle data
+    X, y = shuffle(X, y)
+
+    # number of CV folds
+    k = 10
+
+    # test svm (perform nested k fold cross validation)
+    best_C, best_err, fold_err, total_err, err_dict_svm = validation.kfold(k, X, y, LinearSVC, {"C": [.1, 1, 10]})
+    best_C2, best_err2,fold_err2, total_err2, err_dict_knn = validation.kfold(k, X, y, KNeighborsClassifier, {"n_neighbors": [5,10,15]})
+
+    # return the svm for error dictionary
+    return total_err, total_err2, err_dict_svm, err_dict_knn
+
 
 if __name__ == "__main__":
     # turn off convergence warnings, 
@@ -14,41 +99,12 @@ if __name__ == "__main__":
     if not sys.warnoptions:
         warnings.simplefilter("ignore")
 
-
     # fix seed for testing purposes
     random.seed(10)
 
-    # max size = 2000
-    size = 1000
-    X, y = preprocess.run("review_polarity.tar.gz", 0.05, size, binary=False)
+    # generate 1st experimental results
+    create_hyperparameter_plots()
+    # generate 2nd experiment results
+    create_size_plots()
 
-    y = y.to_numpy(dtype=int)
-
-    # shuffle data
-    X,y = shuffle(X, y)
-
-    # constants 
-    # number of CV folds
-    k = 10
-
-    # test svm (perform nested k fold cross validation)
-    best_C, best_err, fold_err, total_err, err_dict_svm = validation.kfold(k, X, y, LinearSVC, {"C": [.1,1,10]})
-    print("SVM Results:")
-    print(best_C)
-    print(best_err)
-    print(fold_err)
-    print(total_err)
-    print(err_dict_svm)
-
-    # test knn (perform nested k fold cross validation)
-    best_C, best_err,fold_err, total_err, err_dict_knn = validation.kfold(k, X, y, KNeighborsClassifier, {"n_neighbors": [5,10,15]})
-    print("KNN Results:")
-    print(best_C)
-    print(best_err)
-    print(fold_err)
-    print(total_err)
-    print(err_dict_knn)
-
-    # plot results
-    hyperparam_plot('SVM Error for different slack values (C)', 'C', err_dict_svm)
-    hyperparam_plot('KNN Error for different nearest-neighbor values (k)', 'k', err_dict_knn)
+    plt.show()
--- plot.py
+++ plot.py
@@ -12,7 +12,7 @@ def sample_plot(x, y, title):
     plt.xlabel('sample sizes')
     plt.ylabel('cross-val error')
     plt.fill_between(x, (y-cinterval), (y+cinterval), alpha=0.05)
-    plt.show()
+    #plt.show()
 
 def hyperparam_plot(title, xlab, err_dict):
     fig, ax = plt.subplots()
@@ -26,20 +26,22 @@ def hyperparam_plot(title, xlab, err_dict):
     ax.set_ylabel('Error')
     ax.set_xlabel(xlab)
 
-    plt.show()
+    #plt.show()
 
     # return incase we want to make changes after function call
     return fig,ax
 
-
+# used for testing purposes
 if __name__ == "__main__":
     # testing hyperparam plot
     #matplotlib.use('TkAgg')
     #plt.ioff()
     test_data = {0.1: [0.23879590879814067, 0.21828255587819517, 0.23336295833375126, 0.23124525612438757, 0.21880039855057182, 0.22149232135831323, 0.21274808702709, 0.22439998088104285, 0.2127951384813252, 0.21943764304218724], 1: [0.2417847378484875, 0.2187509616870608, 0.23008012190106536, 0.23213278992380357, 0.2314014446806329, 0.22886685756119274, 0.21190215375435956, 0.23146733255126808, 0.21574453495003493, 0.21104110080193542], 10: [0.23804803714600595, 0.21516937954155427, 0.2276390166379559, 0.2360583875095882, 0.22920916033398414, 0.2270999823200929, 0.22669965978808598, 0.22178080381315893, 0.2191448904888333, 0.22111348181901117]}
     hyperparam_plot(title='Test Plot', xlab='C', err_dict=test_data)
+    hyperparam_plot(title='Test Plot', xlab='C', err_dict=test_data)
+    plt.show()
 
     x = np.array([1, 3, 5, 7])
     y = np.array([5, 7, 3, 5])
-    sample_plot(x, y, "random")
+    #sample_plot(x, y, "random")
     #plt.close()
